587b85805467c5e12fd8e87213dfd50853e0eb06
Platinum2d <manicardi215@gmail.com>
2019-05-14 21:53:40 +0200

    Added the author field

--- lib/models/album.dart
+++ lib/models/album.dart
@@ -1,16 +1,20 @@
+import 'package:droptune/models/author.dart';
 import 'package:flutter/material.dart';
 
 import 'package:droptune/models/track.dart';
 
 class Album {
-  Album(
-      {this.id,
-      @required this.coverImage,
-      @required this.name,
-      @required this.tracks});
+  Album({
+    this.id,
+    @required this.coverImage,
+    @required this.name,
+    @required this.author,
+    @required this.tracks,
+  });
 
   final int id;
   final AssetImage coverImage;
   final String name;
+  final Author author;
   final List<Track> tracks;
 }
