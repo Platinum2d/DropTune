75c434f6281a98df811379f1ca4f6281485a21c0
Platinum2d <manicardi215@gmail.com>
2019-07-26 00:49:35 +0200

    Database API implemented

---
+++ lib/misc/database/database_client.dart
@@ -0,0 +1,131 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:droptune/models/track.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseClient {
+  Database _db;
+  Track track;
+
+  Future create() async {
+    Directory path = await getApplicationDocumentsDirectory();
+    String dbpath = join(path.path, "database.db");
+    _db = await openDatabase(dbpath, version: 1, onCreate: this._create);
+  }
+
+  Future _create(Database db, int version) async {
+    await db.execute("""
+    CREATE TABLE tracks(id NUMBER, remoteId NUMBER, name TEXT, duration NUMBER, coverImage TEXT, album TEXT, path TEXT, author TEXT, albumId NUMBER)
+    """);
+  }
+
+  Future<int> insertTrack(Track track) async {
+    int id = 0;
+    var count = Sqflite.firstIntValue(await _db
+        .rawQuery("SELECT COUNT(*) FROM tracks WHERE id = ?", [track.id]));
+    if (count == 0) {
+      id = await _db.insert("tracks", Track.toMap(track));
+    } else {
+      await _db.update("tracks", Track.toMap(track),
+          where: "id= ?", whereArgs: [track.id]);
+    }
+    return id;
+  }
+
+  Future<bool> alreadyLoaded() async {
+    var count = Sqflite.firstIntValue(
+        await _db.rawQuery("SELECT COUNT(*) FROM tracks"));
+    if (count > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<List<Track>> fetchTracks() async {
+    List<Map> results =
+        await _db.query("tracks", columns: Track.columns, orderBy: "name");
+    List<Track> tracks = new List();
+    results.forEach((t) {
+      Track track = Track.fromMap(t);
+      tracks.add(track);
+    });
+    return tracks;
+  }
+
+  Future<List<Track>> fetchTracksFromAlbum(int id) async {
+    List<Map> results =
+        await _db.query("tracks", columns: Track.columns, where: "albumId=$id");
+    List<Track> tracks = new List();
+    results.forEach((s) {
+      Track track = Track.fromMap(s);
+      tracks.add(track);
+    });
+    return tracks;
+  }
+
+  Future<List<Track>> fetchAlbums() async {
+    List<Map> results = await _db.rawQuery(
+        "select distinct albumId, album, author, albumArt from tracks group by album order by album");
+    List<Track> tracks = new List();
+    results.forEach((s) {
+      Track track = Track.fromMap(s);
+      tracks.add(track);
+    });
+    return tracks;
+  }
+
+  Future<List<Track>> fetchArtist() async {
+    List<Map> results = await _db.rawQuery(
+        "select distinct artist, album, albumArt from tracks group by artist order by artist");
+    List<Track> tracks = new List();
+    results.forEach((s) {
+      Track track = Track.fromMap(s);
+      tracks.add(track);
+    });
+    return tracks;
+  }
+
+  Future<List<Track>> fetchTrackByArtist(String artist) async {
+    List<Map> results = await _db.query("tracks",
+        columns: Track.columns, where: "artist='$artist'");
+    List<Track> tracks = new List();
+    results.forEach((s) {
+      Track track = Track.fromMap(s);
+      tracks.add(track);
+    });
+    return tracks;
+  }
+
+  Future<int> updateTrack(Track track) async {
+    int id = 0;
+    // id==9999 for shared track
+    await _db
+        .update("tracks", Track.toMap(track), where: "id= ?", whereArgs: [track.id]);
+
+    return id;
+  }
+
+  Future<List<Track>> searchTrack(String q) async {
+    List<Map> results =
+        await _db.rawQuery("select * from songs where title like '%$q%'");
+    List<Track> tracks = new List();
+    results.forEach((s) {
+      Track track = Track.fromMap(s);
+      tracks.add(track);
+    });
+    return tracks;
+  }
+
+  Future<List<Track>> fetchTracksById(int id) async {
+    List<Map> results = await _db.rawQuery("select * from songs where id=$id");
+    List<Track> tracks = new List();
+    results.forEach((s) {
+      Track track = Track.fromMap(s);
+      tracks.add(track);
+    });
+    return tracks;
+  }
+}
--- lib/models/track.dart
+++ lib/models/track.dart
@@ -4,6 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:droptune/models/author.dart';
 
 class Track {
+  static List<String> columns = [
+    "id",
+    "remoteId",
+    "name",
+    "duration",
+    "coverImage",
+    "album",
+    "path",
+    "author",
+    "albumId"
+  ];
+
   Track(
       {this.id,
       @required this.coverImage,
@@ -12,7 +24,7 @@ class Track {
       @required this.duration,
       @required this.path,
       @required this.author,
-        @required this.album});
+      @required this.album});
 
   final int id;
   final AssetImage coverImage;
@@ -22,5 +34,32 @@ class Track {
   final String path;
   final Author author;
   final Album album;
+
+  static Map<String, dynamic> toMap(Track t) {
+    return {
+      'id': t.id,
+      'coverImage': t.coverImage.assetName,
+      'remoteId': t.remoteId,
+      'name': t.name,
+      'duration': t.duration.inMilliseconds,
+      'path': t.path,
+      'author': t.author.name,
+      'album': t.album.name,
+      'albumId': t.album.id
+    };
+  }
+
+  static Track fromMap(Map<String, dynamic> t) {
+    return Track(
+      id: t["id"],
+      coverImage: AssetImage(t["coverImage"]),
+      remoteId: t["remoteId"],
+      name: t["name"],
+      duration: Duration(milliseconds: t["duration"]),
+      path: t["path"],
+      author: Author(name: t["author"]),
+      album: Album(name: t["album"], id: t["id"]),
+    );
+  }
 /*final User user;*/
 }
