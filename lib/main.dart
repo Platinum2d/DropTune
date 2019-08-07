aa8078cb5d77bb79904da55fec963a3fbde17fe1
Platinum2d <manicardi215@gmail.com>
2019-08-08 00:26:10 +0200

    Removed sample provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:droptune/interfaces/access/access_hub.dart';
 import 'package:droptune/interfaces/access/login.dart';
 import 'package:droptune/interfaces/access/register.dart';
 import 'package:droptune/interfaces/pages/main_page.dart';
+import 'package:droptune/misc/droptune_player.dart';
 import 'package:droptune/misc/get_it_reference.dart';
 import 'package:droptune/misc/no_glow_scroll_behavior.dart';
 import 'package:droptune/misc/permissions/permissions_hub.dart';
@@ -76,10 +77,7 @@ class DroptuneApp extends StatelessWidget {
               border: OutlineInputBorder(
                   borderSide: BorderSide.none,
                   borderRadius: BorderRadius.circular(inputRadius)))),
-      home: Provider<String>(
-        builder: (context) => "io sono una stringa del provider",
-        child: SplashPage(),
-      ),
+      home: SplashPage(),
     );
   }
 }
